aee0529d6cc99c540c07f30150690cd401e423aa
gokel166 <okellogeorge40@yahoo.com>
2018-02-12 19:28:35 -0600

    First elixir chat commit

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,32 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import "phoenix_html";
+import {Socket, Presence} from "phoenix";
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+let user = document.getElementById("user").innerText;
+let socket = new Socket("/socket", {params: {user: user}});
+socket.connect();
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+let presences = {};
 
-// import socket from "./socket"
+let formatedTimestamp = (Ts) => {
+	let date = new Date(Ts);
+	return data.toLocaleString();
+}
+
+let listBy = (user, {metas: metas}) = {
+	return {
+		user: user,
+		onlineAt: formatedTimestamp(metas[0].online_at)
+	};
+}
+
+let userList = document.getElementById("userList");
+let render = (presences) => {
+	userList.innerHTML = Presence.list(presences, listBy)
+		.map(presences => `
+			<li>
+				${presence.user}
+				<br>
+				<small>online since ${presence.onlineAt}</small>
+			</li>
+		`)
+}
